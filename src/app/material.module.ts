deb1127f1af8da64ee376046ae820b7e1c201e77
ingselvi <ing.rodriguez.selvi@gmail.com>
2021-04-03 19:43:08 -0400

    version1

---
+++ src/app/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+
+const myModules=[MatToolbarModule,
+                MatSidenavModule,
+                MatButtonModule,
+                MatMenuModule,
+                MatListModule,
+                MatIconModule];
+
+@NgModule({
+  imports: [...myModules],
+  exports: [...myModules],
+
+})
+
+export class MaterialModule{}
